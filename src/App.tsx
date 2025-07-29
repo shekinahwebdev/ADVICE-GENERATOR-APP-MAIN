38faeda49423e613e543bc81c6c345f990d9a1e3
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-29 23:45:43 +0000

    Implement main app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-
 const App = () => {
   return (
     <main className="main_app">
-      <h1 className="main_title">Advice Generator</h1>
+      <h1 className="main_title">Advice #117</h1>
+
+      <p className="main_subtitle">
+        "it is easy to sit up and take notice, what's difficult is getting up
+        and taking action."
+      </p>
     </main>
   );
 };
