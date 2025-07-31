ede77ac1ad2db6a01a478f7900fdf652237140fd
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-31 22:17:03 +0000

    Styling main app component

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,21 @@ const App = () => {
         "It is easy to sit up and take notice, what's difficult is getting up
         and taking action."
       </p>
-      <picture>
-        <source
-          media="(min-width: 768px)"
-          srcSet="/images/pattern-divider-desktop.svg"
-        />
-        <source
-          media="(max-width: 767px)"
-          srcSet="/images/pattern-divider-mobile.svg"
-        />
-        <button className="switch_button">
-          <img src={diceIcon} alt="dice icon" className="dice_button_icon" />
-        </button>
-      </picture>
+      <div className="divider">
+        <picture>
+          <source
+            media="(min-width: 768px)"
+            srcSet="/images/pattern-divider-desktop.svg"
+          />
+          <source
+            media="(max-width: 767px)"
+            srcSet="/images/pattern-divider-mobile.svg"
+          />
+        </picture>
+      </div>
+      <button className="switch_button">
+        <img src={diceIcon} alt="dice icon" className="dice_button_icon" />
+      </button>
     </main>
   );
 };
