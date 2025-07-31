21483d8ec944da0aeb85b4737c25889c4a4d4786
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-31 20:54:35 +0000

    Add css styling to the main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import diceIcon from "/assets/images/icon-dice.svg";
+import "./App.css";
+
 const App = () => {
   return (
     <main className="main_app">
       <h1 className="main_title">Advice #117</h1>
-
       <p className="main_subtitle">
-        "it is easy to sit up and take notice, what's difficult is getting up
+        "It is easy to sit up and take notice, what's difficult is getting up
         and taking action."
       </p>
-
-      <div className="press_button"></div>
-
-      <div className=""></div>
+      <picture>
+        <source
+          media="(min-width: 768px)"
+          srcSet="/images/pattern-divider-desktop.svg"
+        />
+        <source
+          media="(max-width: 767px)"
+          srcSet="/images/pattern-divider-mobile.svg"
+        />
+        <button className="switch_button">
+          <img src={diceIcon} alt="dice icon" className="dice_button_icon" />
+        </button>
+      </picture>
     </main>
   );
 };
