1ce71a3cc98e8413d257dc865ce0c79748f2bda2
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-01 19:35:42 +0000

    Implement fetch api effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import diceIcon from "/assets/images/icon-dice.svg";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [clicked, setClicked] = useState(false);
+  const [adviceapi, setAdviceApi] = useState(
+    "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+  );
+
+  const fetchApi = async () => {
+    setClicked(true);
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdviceApi(data.slip.advice);
+    } catch (error) {
+      console.error("Failed to fetch api:", error);
+    }
+    setClicked(false);
+  };
   return (
     <main className="main_app">
       <h1 className="main_title">Advice #117</h1>
       <p className="main_subtitle">
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action."
+        {clicked ? "Loading advice..." : `"${adviceapi}"`}
       </p>
       <div className="divider">
         <picture>
@@ -26,7 +42,7 @@ const App = () => {
           />
         </picture>
       </div>
-      <button className="switch_button">
+      <button className="switch_button" onClick={fetchApi}>
         <img src={diceIcon} alt="dice icon" className="dice_button_icon" />
       </button>
     </main>
